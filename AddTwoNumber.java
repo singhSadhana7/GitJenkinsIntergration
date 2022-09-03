4bbda1f836fefa9e6f29f9172f0b77100e9fbff5
singhSadhana7 <sadhanarajput98@gmail.com>
2022-09-03 15:39:53 +0530

    AddTwoNumber

---
+++ AddTwoNumber.java
@@ -0,0 +1,9 @@
+class AddTwoNumber{
+
+public static void main(String args[]){
+
+int firstNumber=Integer.parseInt(args[0]);
+int secondNumber=Integer.parseInt(args[1]);
+System.out.println("sum="+(firstNumber+secondNumber));
+}
+}
